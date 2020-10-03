395036b89d41fc8e90c86c7fd46a0cae0c8dac99
acarrillo2019 <alric21@aol.com>
2020-10-03 04:41:25 -0700

    Functions, Returning Data from Functions

--- TripleX.cpp
+++ TripleX.cpp
@@ -1,11 +1,15 @@
 #include <iostream>
 
-int main()
+void PrintIntroduction()
 {
-    // Print welcome messages to terminal
-    std::cout << "You're a secret agent breaking into a secure server room...";
-    std::cout << std::endl;
-    std::cout << "Enter the correct code to continue..." << std::endl;
+    // Print Intro messages to terminal
+    std::cout << "\n\n You're a secret agent breaking into a secure server room...\n";
+    std::cout << "Enter the correct code to continue...\n\n";
+}
+
+bool PlayGame()
+{
+    PrintIntroduction();
 
     // Declare 3 number code
     const int CodeA = 4;
@@ -17,29 +21,40 @@ int main()
 
     // Print CodeSum and CodeProduct to the terminal
     std::cout << std::endl;
-    std::cout << "There are 3 numbers in the code" << std::endl;
-    std::cout << "The codes add up to: " << CodeSum << std::endl;
-    std::cout << "The codes multiply to give: " << CodeProduct << std::endl;
-    std::cout << CodeSum << std::endl;
-    std::cout << CodeProduct << std::endl;
+    std::cout << "+ There are 3 numbers in the code";
+    std::cout << "\n+ The codes add up to: " << CodeSum;
+    std::cout << "\n+ The codes multiply to give: " << CodeProduct << std::endl;
 
+    // Store Plaer Guess
     int GuessA, GuessB, GuessC;
-    std::cin >> GuessA;
-    std::cin >> GuessB;
-    std::cin >> GuessC;
+    std::cin >> GuessA >> GuessB >> GuessC;
 
     int GuessSum = GuessA + GuessB + GuessC;
     int GuessProduct = GuessA * GuessB * GuessC;
 
+    // Check if the player's guess is correct
     if (GuessSum == CodeSum && GuessProduct == CodeProduct) 
     {
-        std::cout << "You win!";
+        std::cout << "\n You win!";
+        return true;
     }
     else
     {
-        std::cout << "You lose!";
+        std::cout << "\n You lose!";
+        return false;
     }
+}
+
+
+int main()
+{
     
+    while(true)
+    {
+        bool bLevelComplete = PlayGame();
+        std::cin.clear(); //Clears any errors
+        std::cin.ignore(); //Discards the buffer
+    }
 
     return 0;
 }
